c839fd65f26655dc3ebe8fc7d2ab4741b907cb22
unknown <samuelbankole779@gmail.com>
2022-01-14 09:26:05 +0100

    Updates

---
+++ precache-manifest.4b170112b96b734f3370059993ccca4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbf1d57ddf82177a75d8e0f7af07cd3",
+    "url": "/lucky-dude/index.html"
+  },
+  {
+    "revision": "720b4a2a807505e3b7a2",
+    "url": "/lucky-dude/static/css/main.2b5061a3.chunk.css"
+  },
+  {
+    "revision": "92bdc81ad2f4d6e474dc",
+    "url": "/lucky-dude/static/js/2.ca3064fe.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/lucky-dude/static/js/2.ca3064fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720b4a2a807505e3b7a2",
+    "url": "/lucky-dude/static/js/main.2fbba230.chunk.js"
+  },
+  {
+    "revision": "7adc5ea42537c6b9c909",
+    "url": "/lucky-dude/static/js/runtime-main.0322c127.js"
+  }
+]);
